8095fb494f58d50543b72fb072cc88252aa1e72a
Henrik Bjørnskov <henrik@bjrnskov.dk>
2012-07-27 07:38:47 +0200

    Pagodabox

--- web/index.php
+++ web/index.php
@@ -4,9 +4,6 @@ use Gumflap\Application;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-if (false == is_file($file = __DIR__ . '/../config/gumflap.json')) {
-    die('Copy "config/gumflap.json.dist" to "config/gumflap.json" and change the settings to match yours.');
-}
-
-$app = new Application(json_decode(file_get_contents($file), true));
+$configs = json_decode(file_get_contents(__DIR__ . '/../config/gumflap.json'), true);
+$app = new Application($configs);
 $app->run();
